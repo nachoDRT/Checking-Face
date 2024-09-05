6487c3cff11fe8aaf534670ce4c89b74ac1a9b67
nachoDRT <iderodrigo@comillas.edu>
2024-09-05 18:00:46 +0200

    Basic Architecture Layout

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.hfgenerator import HFGenerator
+
+
+def main():
+    human_face_generator = HFGenerator()
+    human_face_generator.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/hfgenerator.py
@@ -0,0 +1,8 @@
+class HFGenerator:
+
+    def __init__(self, render: bool = True) -> None:
+        self.render = render
+
+    def run(self):
+        """Runs the genetor pipeline"""
+        print("Im running")
